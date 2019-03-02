a4a9179c47bc2f192d62e329f3bc737707fe4c3b
Henk0 <iHenk0@yandex.ru>
2019-03-02 18:29:02 +0300

    Classifier and template implemented'

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -45,10 +45,23 @@ def update_news():
     redirect("/news")
 
 
-@route("/classify")
+@route("/recommendations")
 def classify_news():
-    # PUT YOUR CODE HERE
-    return None
+    s = session()
+    classifier = NaiveBayesClassifier()
+    train_news = s.query(News).filter(News.label != None).options(load_only("title", "label")).all()
+    x_train = [row.title for row in train_news]
+    y_train = [row.label for row in train_news]
+    classifier.fit(x_train, y_train)
+    test_news = s.query(News).filter(News.label == None).all()
+    x = [row.title for row in test_news]
+    labels = classifier.predict(x)
+
+    good = [test_news[i] for i in range(len(test_news)) if labels[i] == 'good']
+    maybe = [test_news[i] for i in range(len(test_news)) if labels[i] == 'maybe']
+    never = [test_news[i] for i in range(len(test_news)) if labels[i] == 'never']
+
+    return template('recommendations_template', {'good': good, 'never': never, 'maybe': maybe})
 
 
 if __name__ == "__main__":
